30fa73fd5883ac019352762bd3ebe9149ce9a40b
Ikramul Haque <121812810+IkkuBhai@users.noreply.github.com>
2023-03-05 19:06:50 +0530

    Add files via upload

---
+++ Search.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Search.css";
+import SearchBar from "./Components/SearchBar";
+
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBar placeholder="Search" />
+    </div>
+  );
+}
+
+export default App;
